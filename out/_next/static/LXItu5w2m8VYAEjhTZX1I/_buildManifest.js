a7b4327001bfdd9af6954bff7dbe2095879c2395
e-cesar9 <cesarpetitjean@gmail.com>
2024-01-26 15:12:38 +0100

    deploy

--- out/_next/static/LXItu5w2m8VYAEjhTZX1I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/Me":["static/chunks/pages/Me-a64743c805e382c1.js"],"/_error":["static/chunks/pages/_error-e87e5963ec1b8011.js"],sortedPages:["/Me","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
