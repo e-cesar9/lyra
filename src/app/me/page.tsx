171432bcde391ac22afc3b48ad6fb9b523c5aa87
e-cesar9 <cesarpetitjean@gmail.com>
2024-02-06 10:43:56 +0100

    deploy

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -288,8 +288,8 @@ const DiaryPage: React.FC = () => {
               stagger: 0.05,
               scrollTrigger: {
                 trigger: textRef5.current,
-                start: "top bottom-=200%",
-                end: "bottom+=115% top+=40%",
+                start: "top-=10% bottom-=200%",
+                end: "bottom+=125% top+=40%",
                 scrub: true,
                 markers: true,
 
