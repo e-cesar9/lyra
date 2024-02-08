7c0a6711837f549eb050eed9984d5c52689c5828
e-cesar9 <cesarpetitjean@gmail.com>
2024-02-08 09:08:11 +0100

    deploy

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -53,7 +53,7 @@ const DiaryPage: React.FC = () => {
               scrollTrigger: {
                 trigger: ".echo",
                 start: "start",
-                end: "bottom+=100%",
+                end: "bottom+=80%",
                 scrub: true,
                 // pin: letter,
               },
