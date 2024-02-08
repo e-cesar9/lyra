35a073ec4118faac2a3fb6394ff52245c779a318
e-cesar9 <cesarpetitjean@gmail.com>
2024-02-08 18:34:21 +0100

    deploy

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -221,8 +221,8 @@ const DiaryPage: React.FC = () => {
               filter: "blur(0px)",
               scrollTrigger: {
                 trigger: blocx,
-                start: "top+=80% bottom",
-                end: "top+=35% top",
+                start: "top+=50% bottom",
+                end: "top+=5% top",
                 scrub: true,
               },
             },
