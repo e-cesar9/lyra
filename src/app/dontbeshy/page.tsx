14da2e6243ce1dbebdb45a1bd4cfaadd931fba7a
e-cesar9 <cesarpetitjean@gmail.com>
2024-02-20 11:35:22 +0100

    deploy

--- src/app/dontbeshy/page.tsx
+++ src/app/dontbeshy/page.tsx
@@ -1,20 +1,36 @@
 "use client"
 import React from "react"
-import ChatComponent from "../../components/chatComponent"
-import RootLayout from "../layout"
+import Sidebar from "../sideBar"
+import './style.css'
 
 const DiaryPage = ({}) => {
   const inputRef = React.useRef<HTMLInputElement>(null)
   const containerRef = React.useRef(null)
 
   return (
-    <div
-      ref={containerRef}
-      id="yo"
-      className="overflow-y-auto h-full p-4 rounded layout"
-    >
-      <ChatComponent inputRef={inputRef} containerRef={containerRef} />
-    </div>
+<>
+<Sidebar/>
+<div className="flex flex-col w-full h-full">
+<div className="flex flex-col justify-center align-middle items-center h-full w-full whitespace-pre -mt-8">
+
+<a>SAY HI</a>
+
+<div className="py-16">
+<div className="text-7xl text-center uppercase">
+ No need
+</div>
+
+<div className="text-7xl text-center uppercase">
+ to be shy.
+</div>
+</div>
+
+<a>Let’s talk</a>
+
+</div>
+<div className="flex flex-row px-12 pb-4 justify-between"> <div className="uppercase">hello</div> <div className="uppercase">Social</div> <div className="uppercase">Contacts</div> <div className="uppercase">Location</div> </div>
+</div>
+</>
   )
 }
 
