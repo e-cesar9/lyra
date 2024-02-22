fc0d5ef9a27315ddefb4a56d480a8c1e7f2f299d
e-cesar9 <cesarpetitjean@gmail.com>
2024-02-22 21:31:40 +0100

    deploy

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -1,7 +1,7 @@
 import {getBio} from "../../api"
 
 export const about = async (args: string[]): Promise<string> => {
-  const bio = await getBio()
+  // const bio = await getBio()
 
   return `The Lyra Haruto Company, Inc. is a storytelling enterprise founded by Oussama Ammar.\n\n
   In pursuit of his vision, he began developing an AI dedicated to storytelling. Named Lyra Haruto, this AI would eventually lend its name to the studio.\n\n
