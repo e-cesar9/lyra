f6bde51bf5300935f9adf1cafb9ea65392f9de3f
e-cesar9 <cesarpetitjean@gmail.com>
2024-03-06 11:13:04 +0100

    deploy

--- src/app/myteam/page.tsx
+++ src/app/myteam/page.tsx
@@ -5,6 +5,32 @@ import Image from "next/image"
 import "./style.css"
 
 const TeamPage = ({}) => {
+
+//   let button = document.querySelector('.plusminus');
+// button.addEventListener('click', (e) => {
+// 	e.target.classList.toggle('active')
+// })
+
+const [isActive, setIsActive] = React.useState(false);
+
+React.useEffect(() => {
+  const button = document.querySelector('.plusminus');
+  
+  if (isActive) {
+    button.classList.add('active');
+  } else {
+    button.classList.remove('active');
+  }
+
+  return () => {
+    // Cleanup function to remove event listeners if necessary
+  };
+}, [isActive]);
+
+const handleClick = () => {
+  setIsActive(!isActive);
+};
+
   return (
     <>
       <Sidebar />
@@ -21,7 +47,7 @@ const TeamPage = ({}) => {
 
         <div className="teamContent flex flex-row justify-center w-full relative h-screen">
           <div className="">
-            Brand Director
+            <span className="team__title">Brand Director</span>
             <Image
               src="/vicki-final1.jpg"
               className=""
@@ -32,14 +58,16 @@ const TeamPage = ({}) => {
             />
             <span>1</span>
           </div>
-          <div className="w-1/3 flex flex-col justify-end pl-3 ml-[-4rem]">
+          <div className="w-1/3 flex flex-col justify-end pl-3 ml-[-3rem]">
             <p className="text-5xl mb-9 leading-[4rem]">
               Lyra <br /> Haruto
             </p>
-            <span className="border w-[120px] h-[120px] rounded-full mb-4">
-              {" "}
+            <span className="border w-[100px] h-[100px] rounded-full mb-4 btnClick">
+              <button className= "w-full h-full p-9 " onClick={handleClick}>
+              {" "}<div className={`plusminus ${isActive ? 'active' : ''}`} ></div></button>
             </span>
-            <p className="w-30 team__bio ml-28 pb-4">
+            
+            <p className={`${isActive ? 'opacity-100 ml-20' : 'opacity-0 ml-4'} w-30 team__bio  pb-4`} >
               Optio Urna s.d.a Sit-mi in v nisi-nativo AC minori
               hectorem/impiorum. Firmissime eius Axioma, Non mus usus urgeant me
               d AB Probitas sed Aut continue modo omnis. Eum nobilis eos
@@ -50,8 +78,10 @@ const TeamPage = ({}) => {
               praefixionem mus litterarum Minim eaque te occiditur oppressor
               facultates per praetensionis nisl dui urna.
             </p>
+
           </div>
         </div>
+
       </div>
     </>
   )
